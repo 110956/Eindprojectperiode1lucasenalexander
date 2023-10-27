78409c5ae2f547b2f58943f4e00e900b1f44fce9
11106 <95089285+11106@users.noreply.github.com>
2023-10-27 11:34:44 +0000

    commentaar toegevoegd, code opgeschoond en hartjes locatie teruggezet

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class Raster { //Raster word gemaakt 
+class Raster { // Schepping van het raster
   constructor(r,k) {
     this.aantalRijen = r;
     this.aantalKolommen = k;
@@ -22,7 +22,7 @@ class Raster { //Raster word gemaakt
   }
 }
 
-class Jos { //De schepping van jos
+class Jos { // De schepping van jos
   constructor() {
     this.x = 0;
     this.y = 300;
@@ -34,7 +34,7 @@ class Jos { //De schepping van jos
     this.levens = 1;
   }
   
-  beweeg() { //Bestuuring m.b.v. WASD, achteruit hebben we extra toegevoegd!
+  beweeg() { // Bestuuring m.b.v. WASD
     if (keyIsDown(68)) {
       this.x += this.stapGrootte;
       this.frameNummer = 1;
@@ -87,7 +87,7 @@ class Jos { //De schepping van jos
   }
 }
 
-class Vijand {  //Schepping van vijandige tanks
+class Vijand {  // Schepping van vijandige tanks
   constructor(x,y) {
     this.x = x;
     this.y = y;
@@ -107,8 +107,8 @@ class Vijand {  //Schepping van vijandige tanks
     image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
   }
 }
-var x = [500, 550, 600, 650, 700, 750, 800, 850];
-class Bom { //Schepping van de bommen
+var x = [200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850];
+class Bom { // Schepping van de explosieve de bommen
   constructor(y,vy) {
     this.x = random(x);
     this.y = y;
@@ -128,7 +128,7 @@ class Bom { //Schepping van de bommen
 		        image(this.sprite, this.x, this.y, raster.celGrootte, raster.celGrootte);
 	}
 }
-class Medkit {
+class Medkit { // Schepping van de medische dozen (medkits)
   constructor(x,y) {
     this.x = x;
     this.y = y;
@@ -160,7 +160,7 @@ let medkit;
 let medkit1;
 
 
-function preload() { //repload voor alle afbeeldingen en geluiden die we in dit spel hebben.
+function preload() { //Importeert alle afbeeldingen en geluiden die we in dit spel nodig hebben.
   achtergrond = loadImage("images/backgrounds/informatica playground.jpg");
   dood = loadImage("images/backgrounds/wasted.png");
   gewonnen = loadImage("images/backgrounds/GTA-Mission-Passed.jpg");
@@ -176,17 +176,18 @@ function preload() { //repload voor alle afbeeldingen en geluiden die we in dit 
 }
 
 function setup() { 
-  canvas = createCanvas(900,600); //Creatie spelgebied
+  canvas = createCanvas(900,600); // Creatie spelgebied
   frameRate(10);                  // 10 FPS
   textFont("Verdana");
   textSize(90);
  
-  achtergrondmuziek.loop(); //Speelt muziek af en zorgt dat hij nooit stopt
-  achtergrondmuziek.setVolume(0.2); //Zo dat de muziek niet te hard is in volume
+  achtergrondmuziek.loop(); // Speelt muziek af en zorgt dat hij nooit stopt
+  achtergrondmuziek.setVolume(0.2); // Zo dat de muziek niet te hard is in volume
   
   raster = new Raster(12,18); //Raster groote 12 bij 18 gemaakt
   raster.berekenCelGrootte();
   
+  // attributen toevoegen aan het spel
   eve = new Jos();
   eve.stapGrootte = 1*raster.celGrootte;
   for (var b = 0;b < 6;b++) {
@@ -215,13 +216,33 @@ function setup() {
 	bom3.sprite = loadImage("images/sprites/bomb.png");
 
   bom4 = new Bom(700, raster.celGrootte);
-  bom4.stapGrootte = 5 * raster.celGrootte;
+  bom4.stapGrootte = 2 * raster.celGrootte;
   bom4.sprite = loadImage("images/sprites/bomb.png");
 
-  bom5 = new Bom(500, raster.celGrootte);
-  bom5.stapGrootte = 4 * raster.celGrootte;
+  bom5 = new Bom(800, raster.celGrootte);
+  bom5.stapGrootte = 3 * raster.celGrootte;
   bom5.sprite = loadImage("images/sprites/bomb.png");
 
+  bom6 = new Bom(900, raster.celGrootte);
+  bom6.stapGrootte = 4 * raster.celGrootte;
+  bom6.sprite = loadImage("images/sprites/bomb.png");
+
+  bom7 = new Bom(600, raster.celGrootte);
+  bom7.stapGrootte = 5 * raster.celGrootte;
+  bom7.sprite = loadImage("images/sprites/bomb.png");
+
+  bom8 = new Bom(100, raster.celGrootte);
+  bom8.stapGrootte = -1 * raster.celGrootte;
+  bom8.sprite = loadImage("images/sprites/bomb.png");
+
+  bom9 = new Bom(500, raster.celGrootte);
+  bom9.stapGrootte = -2 * raster.celGrootte;
+  bom9.sprite = loadImage("images/sprites/bomb.png");
+
+  bom10 = new Bom(150, raster.celGrootte);
+  bom10.stapGrootte = -3 * raster.celGrootte;
+  bom10.sprite = loadImage("images/sprites/bomb.png");
+  
   medkit = new Medkit(150, 400);
   medkit.sprite = loadImage("images/sprites/medkit2.png")
 
@@ -245,45 +266,55 @@ function draw() {
 	bom3.beweeg();
   bom4.beweeg();
   bom5.beweeg();
+  bom6.beweeg();
+  bom7.beweeg();
+  bom8.beweeg();
+  bom9.beweeg();
+  bom10.beweeg();
   bob.toon();
 	bom1.toon();
 	bom2.toon();
 	bom3.toon();
   bom4.toon();
   bom5.toon();
+  bom6.toon();
+  bom7.toon();
+  bom8.toon();
+  bom9.toon();
+  bom10.toon();
   
   if(medkit.available){
   medkit.toon();}
   if(medkit1.available1){
     medkit1.toon();}
   raster.teken();
-  if (eve.levens == 1){
-    image(leven1, 00, 00, 50, 50)
+  if (eve.levens == 1){ // 1 hartje voor 1 leven
+    image(leven1, 10, 10, 50, 50)
   }
-  if (eve.levens == 2){
-    image(leven2, 00, 00, 100, 50)
+  if (eve.levens == 2){ // 2 hartjes voor 2 levens
+    image(leven2, 10, 10, 100, 50)
   }
-  if (eve.levens == 3){
-    image(leven3, 00, 00, 150, 50)
+  if (eve.levens == 3){ // 3 hartjes voor 3 levens
+    image(leven3, 10, 10, 150, 50)
   }
   
-  if (eve.aanDeBeurt) {
+  if (eve.aanDeBeurt) { // Als jos (eve) aan de beurt is kan je bewegen
     eve.beweeg();
   }
-  else {
+  else { // Tanks bewegen als je niet aan de beurt bent
     alice.beweeg();
     bob.beweeg();
     eve.aanDeBeurt = true;
   }
   
   if (alice.x == bob.x && alice.y == bob.y) {
-    bob.beweeg();
+    bob.beweeg(); // Als tanks op dezelfde plek zijn beweegt eentje nog een keer
   }
   
   
   
   if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob)) {
-    if (eve.levens == 0){
+    if (eve.levens == 0){ // Checkt voor doodgaan door tanks
       achtergrondmuziek.stop();
       background(dood);
       tankschot.play();
@@ -294,8 +325,8 @@ function draw() {
     }
   }
   
-  if (eve.wordtGeraakt(bom1) || eve.wordtGeraakt(bom2) || eve.wordtGeraakt(bom3) ||eve.wordtGeraakt(bom4) || eve.wordtGeraakt(bom5)) { 
-    if (eve.levens == 0){
+  if (eve.wordtGeraakt(bom1) || eve.wordtGeraakt(bom2) ||                    eve.wordtGeraakt(bom3) || eve.wordtGeraakt(bom4) ||                    eve.wordtGeraakt(bom5) || eve.wordtGeraakt(bom6) ||                    eve.wordtGeraakt(bom7) || eve.wordtGeraakt(bom8) ||                    eve.wordtGeraakt(bom9) || eve.wordtGeraakt(bom10)) { 
+    if (eve.levens == 0) {// Checkt voor doodgaan bommen
       noLoop();
       achtergrondmuziek.stop();
       background(backgroundnuke);
@@ -307,21 +338,21 @@ function draw() {
 
   }
   if (eve.KomtOpMedkit(medkit)) {
-    if(medkit.available){
+    if(medkit.available){ // Checkt voor op medkit staan
       achtergrondmuziek.pause();
       levenerbij.setVolume(0.2);
         levenerbij.play();
-        //Wacht 1200 millie seconden voordat de achtergrondmuziek verder gaat.
+        //Wacht 1200 milli seconden voordat de achtergrondmuziek verder gaat.
         setTimeout(function() {
           achtergrondmuziek.play();
         }, 1200); 
         eve.levens += 1;
       }
-      medkit.verwijder();
+      medkit.verwijder(); // Verwijdert medkit
   }
 
   if (eve.KomtOpMedkit(medkit1)) {
-    if(medkit1.available1){
+    if(medkit1.available1){ // Checkt voor op nedkit1 staan
       achtergrondmuziek.pause();
       levenerbij.setVolume(0.2);
         levenerbij.play();
@@ -331,32 +362,13 @@ function draw() {
         }, 1200);
         eve.levens += 1;
       }
-    medkit1.verwijder1();
+    medkit1.verwijder1(); // Verwijdert medkit1
   }
   
-  if (eve.gehaald) {
-     if (keyIsDown(32)){
-       eve.gehaald = false;
-       eve.levens = 1;
-       eve.aanDeBeurt = true;
-       preload();
-       setup();
-       draw();
-     }
-    else{
-      noLoop();
-      achtergrondmuziek.stop();
-      background(gewonnen);
-      gewonnen2.play();
-    }
+  if (eve.gehaald) { // Checkt of je hebt gewonnen
+    noLoop();
+    achtergrondmuziek.stop();
+    background(gewonnen);
+    gewonnen2.play();
   }
-  //if (eve.gehaald == true || eve.levens == 0 && keyIsDown(32)){
-    //eve.gehaald = false;
-    //eve.levens = 1;
-    //eve.aanDeBeurt = true;
-    //preload();
-   // setup();
-   // draw();
- // }
 }
-
